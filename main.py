24ca6312ca58306bebe3868f0db94c8efdc3e0d3
Kogby <kogby0507@gmail.com>
2024-01-20 23:57:18 +0800

    [update] teaching version

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("This is the old version.")
+def add(a: int, b: int) -> int:
+    return a - b
+
+def multiply(a: int, b: int) -> int:
+    pass
+
+if __name__ == "__main__":
+    print("Please input the calculation you want: ")
+    mode = input()
+    print("Input the first number: ")
+    a = input()
+    print("Input the second number: ")
+    b = input()
+    if mode == "add":
+        print(f"The sum of {a} and {b} is: {add(a, b)}")
+    elif mode == "multiply":
+        print(f"The multiplication of {a} and {b} is: {multiply(a, b)} ")
